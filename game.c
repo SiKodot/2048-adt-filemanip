31402ff7c57fe77a87342582235386a836e8cad2
SiKodot <ihsangamersfauzi123@gmail.com>
2024-11-18 11:51:10 +0700

    v2.23 (fix bug)

--- game.c
+++ game.c
@@ -2,29 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 
-int papan[3][3];
-void resetPapan()
-{
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 0; j < 4; j++)
-        {
-            papan[i][j] = ' ';
+int papan[6][6];
+
+void resetPapan(int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            papan[i][j] = 0; // Menginisialisasi papan dengan 0
         }
     }
 }
 
-void menampilkanPapan(int size)
-{
-    printf("========2048  Game=======\n");
-    printf("-------------------------\n");
-    printf("|  %d  |  %d  |  %d  |  %d  |\n", papan[0][0], papan[0][1], papan[0][2], papan[0][3]);
-    printf("-------------------------\n");
-    printf("|  %d  |  %d  |  %d  |  %d  |\n", papan[1][0], papan[1][1], papan[1][2], papan[1][3]);
-    printf("-------------------------\n");
-    printf("|  %d  |  %d  |  %d  |  %d  |\n", papan[2][0], papan[2][1], papan[2][2], papan[2][3]);
-    printf("-------------------------\n");
-    printf("|  %d  |  %d  |  %d  |  %d  |\n", papan[3][0], papan[3][1], papan[3][2], papan[3][3]);
-    printf("-------------------------\n");
-
+void menampilkanPapan(int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d ", papan[i][j]); // Menampilkan isi papan
+        }
+        printf("\n"); // Baris baru setelah setiap baris papan
+    }
 }
