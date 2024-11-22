b6622446cfd9b52ae59eda54ab5c118553645456
BlvaGP <belva.a.hidayat@gmail.com>
2024-11-22 23:11:48 +0700

    v2.41 (fix prosedur addrandom)

--- game.c
+++ game.c
@@ -10,9 +10,9 @@ void gameMain(){
     int A;
     resetPapan(); // Reset papan sekali di awal
     while (1) { // Loop tak terbatas untuk terus menerima input
+        system("cls");
         menampilkanPapan(); // Tampilkan papan saat ini
         addrandom(); // Tambahkan angka acak di papan
-        menampilkanPapan(); // Tampilkan papan setelah menambahkan angka acak
         geser(&A); // Ambil input dari pengguna
         geserangka(A); // Geser angka berdasarkan input
     }
@@ -26,8 +26,6 @@ void resetPapan() {
             papan[i][j] = 0; // Menginisialisasi papan dengan 0
         }
     }
-    papan[0][3] = 2; // Menambahkan angka 2 di posisi tertentu
-    papan[0][1] = 2;
 }
 
 void menampilkanPapan() {
@@ -74,16 +72,42 @@ void geser(int *arah){
 }
 
 void addrandom (){
+    //inisialisasi generator pengacak angka
+    srand((unsigned)time(NULL));
+
+    int kosong[16][2]; //array untuk menyimpan posisi papan yang kosong
+    int count = 0;
+    
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
             if (papan[i][j] == 0) {
-                papan[i][j] = (rand() %100 <90) ? 2:4;
-            }; // Menginisialisasi papan dengan 0
+                kosong[count][0] = i; //menyimpan baris yang kosong
+                kosong[count][1] = j; //menyimpan kolom yang kosong
+                count ++;
+            } 
         }
     }
+
+    if (count == 0) 
+    {
+        return; //keluar dari prosedur jika tidak ada array yang kosong
+    }
+
+    //pilih posisi array kosong secara acak
+    int pilihan = rand() % count;
+    int baris = kosong[pilihan][0];
+    int kolom = kosong[pilihan][1];
+
+    //tambahkan array kosong yang terpilih dengan angka 2 (90%) atau 4 (10%)
+    papan[baris][kolom] = (rand() % 100 < 90 ? 2 : 4);
+
 }
 
 void geserangka(int arah) {
+    if (arah == 1) {
+        
+    }
+    
     if (arah == 3) { // Kiri
         for (int i = 0; i < 4; i++) {
             for (int j = 1; j < 4; j++) { // Mulai dari kolom 1
@@ -97,5 +121,5 @@ void geserangka(int arah) {
                 }
             }
         }
-}
+    }
 }
